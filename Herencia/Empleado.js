3e91d039d09c5ebeb0c785f151bf4af10ee11bd8
maritza1013 <maritzamae13@gmail.com>
2023-07-13 15:55:19 -0500

    -ejercicio sencillo de herencia en js

---
+++ Herencia/Empleado.js
@@ -0,0 +1,20 @@
+class Empleado extends Persona {
+    static contadorEmpleados;
+    constructor(sueldo){
+        this._idEmpleados = ++Empleado.contadorEmpleados;
+        this._sueldo = sueldo
+    }
+    get idEmpleados(){
+        return this._idEmpleados
+    }
+    get sueldo() {
+        return this._sueldo
+    }
+    set sueldo(sueldo){
+        this._sueldo = sueldo
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idEmpleados} ${this._sueldo}` 
+    }
+}
